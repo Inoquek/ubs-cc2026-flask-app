9788db1710b2197ec97204200a0aa362fca16c54
AkezhanSh <ashakhsbdin@rivermap.com.hk>
2025-09-07 07:14:21 +0800

    trivia

--- routes/trivia.py
+++ routes/trivia.py
@@ -9,7 +9,7 @@ from routes import app
 # hz naschest 6,7,
 # definitely correct 3,1,2,2,3, ... , 4 , 5, 4,3,3, , ,  
 #hz nascchest q.15 now - rn it's 2
-_RAW = os.getenv("TRIVIA_ANSWERS", "3,1,2,2,3,4,4,5,4,3,3,3,      4,    1,2 ,1, 1,2, -1, 1,-1,2,       3,4,2")
+_RAW = os.getenv("TRIVIA_ANSWERS", "3,1,2,2,3,4,4,5,4,3,3,3,      4,    1,2 ,1, 1,2, -1, 1,     4 ,      2,3,4,2")
 
 # new changes (not confident in them)
 # Q.24 - 4 IS CORRECT
@@ -21,7 +21,9 @@ _RAW = os.getenv("TRIVIA_ANSWERS", "3,1,2,2,3,4,4,5,4,3,3,3,      4,    1,2 ,1, 
 # Q.23 - 3 IS CORRECT
 
 # Q.13 - not 1, 2,3
-# TRYING Q.13
+# Q.21 - rn 4
+
+# TRYING Q.13, Q.21
 
 # -1 if dk the answer yet
 # everything else is correct so far
